30fb414a6f6959b87a3e012f6bdb18d36cb21931
Jason Cheatham <j.cheatham@gmail.com>
2020-04-24 08:02:15 -0400

    Initial commit

---
+++ src/test/java/com/example/esexample/EsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
